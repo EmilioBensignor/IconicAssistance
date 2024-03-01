ac5ec743861b9b45e6870d5cbec1191a44545931
Emilio Bensignor <liobensignor@gmail.com>
2024-03-01 14:15:14 -0300

    Testeo Workspaces

--- src/components/CalendlyComponent.vue
+++ src/components/CalendlyComponent.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="calendly-embed" style="width: 70vw; height: 85vh; margin: 5vw 0; height: 700px"></div>
+  <!-- <div
+    class="calendly-inline-widget"
+    data-url="https://calendly.com/iconic-assistants/15min"
+    style="width:70vw; height: 85vh; margin:5vw 0;"></div> -->
+    <button v-on:click="calendlyPopUp">
+
+    </button>
 </template>
 
 <script>
 export default {
-  mounted() {
-    this.loadCalendlyScript();
-  },
   methods: {
-    loadCalendlyScript() {
-      const script = document.createElement('script');
-      script.src = 'https://assets.calendly.com/assets/external/widget.js';
-      script.async = true;
-      script.onload = () => {
-        this.initCalendly();
-      };
-      document.body.appendChild(script);
-    },
-    initCalendly() {
-      if (window.Calendly) {
-        window.Calendly.initInlineWidget({
-          url: 'https://calendly.com/ivan-iconicassistance/15min?hide_gdpr_banner=1',
-          parentElement: document.getElementById('calendly-embed'),
-          prefill: {},
-          utm: {}
-        });
-      }
+    calendlyPopUp(){
+      console.log("Calendly");
     }
   }
-};
+}
 </script>
