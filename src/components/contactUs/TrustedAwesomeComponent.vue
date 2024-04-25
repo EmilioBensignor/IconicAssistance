9a90c61ea30af0a5442cee1e1f0284ef4e9d0f6b
Lio <liobensignor@gmail.com>
2024-04-25 16:33:20 -0300

    Width Reviews

--- src/components/contactUs/TrustedAwesomeComponent.vue
+++ src/components/contactUs/TrustedAwesomeComponent.vue
@@ -7,7 +7,7 @@
           <v-carousel-item>
             <div class="carouselContent d-flex flex-row ga-5">
               <div v-for="(reviewDesktop, index) in reviews.slice(0, 3)" :key="index"
-                class="columnAlignCenter bg-white rounded-xl elevation-5 px-3 py-5">
+                class="columnAlignCenter bg-white rounded-xl elevation-5 pa-5">
                 <img src="@/assets/images/contactUs/Stars-Slide.png" alt="Blue Stars" width="50%">
                 <p class="text-left mt-5" v-html="reviewDesktop"></p>
               </div>
@@ -16,7 +16,7 @@
           <v-carousel-item>
             <div class="carouselContent d-flex flex-row ga-5">
               <div v-for="(reviewDesktop, index) in reviews.slice(3, 6)" :key="index"
-                class="columnAlignCenter bg-white rounded-xl elevation-5 px-3 py-5">
+                class="columnAlignCenter bg-white rounded-xl elevation-5 pa-5">
                 <img src="@/assets/images/contactUs/Stars-Slide.png" alt="Blue Stars" width="50%">
                 <p class="text-left mt-5" v-html="reviewDesktop"></p>
               </div>
@@ -25,7 +25,7 @@
           <v-carousel-item>
             <div class="carouselContent d-flex flex-row ga-5">
               <div v-for="(reviewDesktop, index) in reviews.slice(6, 9)" :key="index"
-                class="columnAlignCenter bg-white rounded-xl elevation-5 px-3 py-5">
+                class="columnAlignCenter bg-white rounded-xl elevation-5 pa-5">
                 <img src="@/assets/images/contactUs/Stars-Slide.png" alt="Blue Stars" width="50%">
                 <p class="text-left mt-5" v-html="reviewDesktop"></p>
               </div>
@@ -36,7 +36,7 @@
           <v-carousel-item>
             <div class="carouselContent d-flex flex-row ga-5">
               <div v-for="(reviewDesktop, index) in reviews.slice(0, 2)" :key="index"
-                class="columnAlignCenter bg-white rounded-xl elevation-5 px-3 py-5">
+                class="columnAlignCenter bg-white rounded-xl elevation-5 pa-5">
                 <img src="@/assets/images/contactUs/Stars-Slide.png" alt="Blue Stars" width="60%">
                 <p class="text-left mt-5" v-html="reviewDesktop"></p>
               </div>
@@ -45,7 +45,7 @@
           <v-carousel-item>
             <div class="carouselContent d-flex flex-row ga-5">
               <div v-for="(reviewDesktop, index) in reviews.slice(2, 4)" :key="index"
-                class="columnAlignCenter bg-white rounded-xl elevation-5 px-3 py-5">
+                class="columnAlignCenter bg-white rounded-xl elevation-5 pa-5">
                 <img src="@/assets/images/contactUs/Stars-Slide.png" alt="Blue Stars" width="60%">
                 <p class="text-left mt-5" v-html="reviewDesktop"></p>
               </div>
@@ -54,7 +54,7 @@
           <v-carousel-item>
             <div class="carouselContent d-flex flex-row ga-5">
               <div v-for="(reviewDesktop, index) in reviews.slice(5, 7)" :key="index"
-                class="columnAlignCenter bg-white rounded-xl elevation-5 px-3 py-5">
+                class="columnAlignCenter bg-white rounded-xl elevation-5 pa-5">
                 <img src="@/assets/images/contactUs/Stars-Slide.png" alt="Blue Stars" width="60%">
                 <p class="text-left mt-5" v-html="reviewDesktop"></p>
               </div>
@@ -63,7 +63,7 @@
           <v-carousel-item>
             <div class="carouselContent d-flex flex-row ga-5">
               <div v-for="(reviewDesktop, index) in reviews.slice(8, 10)" :key="index"
-                class="columnAlignCenter bg-white rounded-xl elevation-5 px-3 py-5">
+                class="columnAlignCenter bg-white rounded-xl elevation-5 pa-5">
                 <img src="@/assets/images/contactUs/Stars-Slide.png" alt="Blue Stars" width="60%">
                 <p class="text-left mt-5" v-html="reviewDesktop"></p>
               </div>
@@ -74,7 +74,7 @@
           class="h-100 carousel mobileCarousel columnAlignCenter py-5">
           <v-carousel-item v-for="(reviewMobile, index) in reviews" :key="index">
             <div class="carouselContent">
-              <div class="columnAlignCenter bg-white rounded-xl elevation-5 px-3 py-5">
+              <div class="columnAlignCenter bg-white rounded-xl elevation-5 pa-5">
                 <img src="@/assets/images/contactUs/Stars-Slide.png" alt="Blue Stars" width="50%">
                 <p class="text-left mt-5" v-html="reviewMobile"></p>
               </div>
@@ -100,16 +100,16 @@ export default {
   data() {
     return {
       reviews: [
-        `<p>"My executive assistant from <span class="text-radioactive">ICONIC has been a game-changer for my productivity.</span> Always efficient and organized, they handle tasks seamlessly, allowing me to focus on what matters most."</p>`,
-        `<p>"I can't imagine managing my schedule without my executive assistant from ICONIC. <span class="text-radioactive">They're responsive, reliable, and go above and beyond</span> to ensure everything runs smoothly."</p>`,
-        `<p>"Thanks to my executive assistant from ICONIC, I've been able to <span class="text-radioactive">delegate tasks with confidence. Their attention to detail and proactive approach make them an invaluable asset</span> to my team."</p>`,
-        `<p>"I've been thoroughly impressed with the level of support provided by my executive assistant from ICONIC. <span class="text-radioactive">They handle everything with precision and professionalism</span>, allowing me to focus on growing my business."</p>`,
-        `<p>"My executive assistant from ICONIC has been a lifesaver. <span class="text-radioactive">They're proactive, resourceful, and always one step ahead</span>. I couldn't imagine navigating my day-to-day without their support."</p>`,
-        `<p>"I highly recommend ICONIC's executive assistant services. <span class="text-radioactive">My assistant is reliable, efficient, and has helped me stay organized</span> and on top of my workload."</p>`,
-        `<p>"Having an <span class="text-radioactive">executive assistant from ICONIC has been a game-changer for my business</span>. They handle all the administrative tasks with ease, allowing me to focus on strategic initiatives."</p>`,
-        `<p>"My experience with <span class="text-radioactive">ICONIC's executive assistant service has been nothing short of excellent</span>. My assistant is responsive, attentive, and consistently delivers results."</p>`,
-        `<p>"I can't thank my executive assistant from ICONIC enough for their support. <span class="text-radioactive">They've truly become an indispensable part of my team</span>, and I'm grateful for their dedication and professionalism."</p>`,
-        `<p>"Working with my executive assistant from ICONIC has been a breeze. They're <span class="text-radioactive">incredibly professional, and their ability to anticipate my needs has saved me</span> countless hours of work."</p>`,
+        `<p class="w-100">"My executive assistant from <span class="text-radioactive">ICONIC has been a game-changer for my productivity.</span> Always efficient and organized, they handle tasks seamlessly, allowing me to focus on what matters most."</p>`,
+        `<p class="w-100">"I can't imagine managing my schedule without my executive assistant from ICONIC. <span class="text-radioactive">They're responsive, reliable, and go above and beyond</span> to ensure everything runs smoothly."</p>`,
+        `<p class="w-100">"Thanks to my executive assistant from ICONIC, I've been able to <span class="text-radioactive">delegate tasks with confidence. Their attention to detail and proactive approach make them an invaluable asset</span> to my team."</p>`,
+        `<p class="w-100">"I've been thoroughly impressed with the level of support provided by my executive assistant from ICONIC. <span class="text-radioactive">They handle everything with precision and professionalism</span>, allowing me to focus on growing my business."</p>`,
+        `<p class="w-100">"My executive assistant from ICONIC has been a lifesaver. <span class="text-radioactive">They're proactive, resourceful, and always one step ahead</span>. I couldn't imagine navigating my day-to-day without their support."</p>`,
+        `<p class="w-100">"I highly recommend ICONIC's executive assistant services. <span class="text-radioactive">My assistant is reliable, efficient, and has helped me stay organized</span> and on top of my workload."</p>`,
+        `<p class="w-100">"Having an <span class="text-radioactive">executive assistant from ICONIC has been a game-changer for my business</span>. They handle all the administrative tasks with ease, allowing me to focus on strategic initiatives."</p>`,
+        `<p class="w-100">"My experience with <span class="text-radioactive">ICONIC's executive assistant service has been nothing short of excellent</span>. My assistant is responsive, attentive, and consistently delivers results."</p>`,
+        `<p class="w-100">"I can't thank my executive assistant from ICONIC enough for their support. <span class="text-radioactive">They've truly become an indispensable part of my team</span>, and I'm grateful for their dedication and professionalism."</p>`,
+        `<p class="w-100">"Working with my executive assistant from ICONIC has been a breeze. They're <span class="text-radioactive">incredibly professional, and their ability to anticipate my needs has saved me</span> countless hours of work."</p>`,
       ],
     }
   },
@@ -134,8 +134,8 @@ import { scrollBottom } from "@/motions.js";
   padding: 3% 10%;
 }
 
-.v-window__controls {
-  padding: 0 !important;
+.carouselContent p{
+  width: 100% !important;
 }
 
 @media only screen and (min-width: 600px) {
