e705cee0ace9a5edb735982297d90bf18d79f3a1
Lio <liobensignor@gmail.com>
2024-04-29 16:46:17 -0300

    Formulario en App

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,32 +1,27 @@
 <template>
   <h1>Test</h1>
+  <div ref="formContainer" id="form-container"></div>
 </template>
 
 <script>
 export default {
   mounted() {
-    // Crear el elemento <script> para cargar el formulario de HubSpot
     const script = document.createElement('script');
     script.setAttribute('charset', 'utf-8');
     script.setAttribute('type', 'text/javascript');
     script.src = '//js.hsforms.net/forms/embed/v2.js';
     script.async = true;
     
-    // Agregar el elemento <script> al final del body
-    document.body.appendChild(script);
+    this.$refs.formContainer.appendChild(script);
     
-    // Llamar a la función de creación del formulario de HubSpot
-    script.onload = function() {
+    script.onload = () => {
       window.hbspt.forms.create({
         region: "na1",
         portalId: "46001660",
-        formId: "d164c3b6-b2b5-4fc9-acdf-b0920ec87420"
+        formId: "d164c3b6-b2b5-4fc9-acdf-b0920ec87420",
+        target: '#form-container'
       });
     };
   }
 };
 </script>
-
-<style scoped>
-/* Aquí puedes incluir estilos específicos para este componente si es necesario */
-</style>
