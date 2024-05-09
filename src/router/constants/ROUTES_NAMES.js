8e35661c0a1d7f1da9a83eb18c90e3c094608c0f
Emilio Bensignor <liobensignor@gmail.com>
2024-05-09 13:45:47 -0300

    Reset Password

--- src/router/constants/ROUTES_NAMES.js
+++ src/router/constants/ROUTES_NAMES.js
@@ -9,6 +9,6 @@ export default {
 	ADD_PAYMENT_METHOD: "/suite/add-payment-method",
 	ADD_PAYMENT_METHOD_CONFIRMATION: "/suite/add-payment-method-confirmation",
 	PAYMENT_METHODS: "/suite/payment-methods",
-	RESET_PASSWORD: "/reset-password",
-	RESET_PASSWORD_CONFIRMATION: "/reset-password-confirmation",
+	RESET_PASSWORD: "/suite/reset-password",
+	RESET_PASSWORD_CONFIRMATION: "/suite/reset-password-confirmation",
 };
