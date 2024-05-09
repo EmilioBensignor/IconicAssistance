3f01f7043426a21a32aea2a3102b0de0cea9f77a
Emilio Bensignor <liobensignor@gmail.com>
2024-05-09 11:50:53 -0300

    Icono Account

--- src/suite/components/HeaderSuiteComponent.vue
+++ src/suite/components/HeaderSuiteComponent.vue
@@ -1,13 +1,27 @@
 <template>
   <nav class="mb-15">
-    <v-app-bar elevation="0" app class="bg-white d-flex align-center py-1">
-      <v-app-bar-nav-icon icon="mdi-menu" color="radioactive" class="hamburger shadow-15" size="x-large"
-        @click="showMenu = !showMenu"></v-app-bar-nav-icon>
+    <v-app-bar
+      elevation="0"
+      app
+      class="bg-white d-flex justify-space-between align-center py-1">
+      <v-app-bar-nav-icon
+        icon="mdi-menu"
+        color="radioactive"
+        class="hamburger shadow-15"
+        size="x-large"
+        @click="showMenu = !showMenu">
+      </v-app-bar-nav-icon>
       <v-toolbar-title>
         <router-link class="text-decoration-none" :to="'/'">
-          <img width="75%" src="@/suite/assets/images/Suite-Logo.png" alt="Suite Logo">
+          <img
+            width="75%"
+            src="@/suite/assets/images/Suite-Logo.png"
+            alt="Suite Logo" />
         </router-link>
       </v-toolbar-title>
+      <div class="account bg-radioactive elevation-3 rounded-circle pa-1">
+        <v-icon color="white" size="x-large" icon="mdi-account"></v-icon>
+      </div>
     </v-app-bar>
     <v-navigation-drawer v-model="showMenu" app class="mt-2">
       <v-list class="py-0" tag="ul">
@@ -27,55 +41,58 @@
 </template>
 
 <script>
-import routes from "@/router/constants/ROUTES_NAMES";
+  import routes from "@/router/constants/ROUTES_NAMES";
 
-export default {
-  name: "HeaderSuiteComponent",
-  data() {
-    return {
-      showMenu: false,
-      suiteMenu: [
-        {
-          path: routes.SUITE,
-          title: "DASHBOARD",
-          icon: "home"
-        },
-        {
-          path: routes.ASSISTANTS,
-          title: "ASSISTANS",
-          icon: "account-group"
-        },
-        {
-          path: routes.INVOICES,
-          title: "INVOICES",
-          icon: "receipt"
-        },
-      ],
-    }
-  },
-  mounted() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
-  beforeDestroy() {
-    window.removeEventListener("scroll", this.handleScroll);
-  },
-  methods: {
-    handleScroll() {
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-      const windowHeight = window.innerHeight;
-
-      const threshold = windowHeight * 0.5;
-
-      if (scrollTop >= threshold) {
-        this.isScrolled = true;
-      } else {
-        this.isScrolled = false;
-      }
+  export default {
+    name: "HeaderSuiteComponent",
+    data() {
+      return {
+        showMenu: false,
+        suiteMenu: [
+          {
+            path: routes.SUITE,
+            title: "DASHBOARD",
+            icon: "home",
+          },
+          {
+            path: routes.ASSISTANTS,
+            title: "ASSISTANS",
+            icon: "account-group",
+          },
+          {
+            path: routes.INVOICES,
+            title: "INVOICES",
+            icon: "receipt",
+          },
+        ],
+      };
     },
-  },
-}
+    mounted() {
+      window.addEventListener("scroll", this.handleScroll);
+    },
+    beforeDestroy() {
+      window.removeEventListener("scroll", this.handleScroll);
+    },
+    methods: {
+      handleScroll() {
+        const scrollTop =
+          window.pageYOffset || document.documentElement.scrollTop;
+        const windowHeight = window.innerHeight;
+
+        const threshold = windowHeight * 0.5;
+
+        if (scrollTop >= threshold) {
+          this.isScrolled = true;
+        } else {
+          this.isScrolled = false;
+        }
+      },
+    },
+  };
 </script>
 
 <style scoped>
+  .account {
+    cursor: pointer;
+  }
 </style>
