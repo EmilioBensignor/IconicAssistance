7186f6e611bddca157b0d60e9ef114d4486353fb
Lio <liobensignor@gmail.com>
2024-06-06 15:58:26 -0300

    Thank You

--- src/web/views/ThankYouView.vue
+++ src/web/views/ThankYouView.vue
@@ -1,3 +1,19 @@
 <template>
-  <h2>Thank you</h2>
+  <div class="h-100 column align-center justify-space-between">
+    <iframe src="https://iconicassistants.na.chilipiper.com/router/disco_call" width="560" height="700"></iframe>
+    <div class="columnAlignCenter">
+      <FooterRightsComponent />
+    </div>
+  </div>
 </template>
+
+<script>
+import FooterRightsComponent from "../components/FooterRightsComponent.vue";
+
+export default {
+  name: "Thank You",
+  components: {
+    FooterRightsComponent,
+  }
+}
+</script>
