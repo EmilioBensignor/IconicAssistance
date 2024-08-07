044b8e10c7755ae2cb4bcb5deb27f03a6975f848
Mark Mysler <myslermark@gmail.com>
2024-08-07 12:40:51 -0300

    vercel edge middleware for geoblocking

---
+++ middleware/_block_traffic.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "@vercel/edge";
+
+export function middleware(req) {
+	const { geo } = req;
+	const allowedContinents = ["NA", "SA", "EU"]; // North America, South America, Europe
+
+	if (!allowedContinents.includes(geo?.continent)) {
+		return new NextResponse("Access Denied", { status: 403 });
+	}
+
+	return NextResponse.next();
+}
